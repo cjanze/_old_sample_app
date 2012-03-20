96cfafd4266cf078f488d1f98baf474f273d8181
Christian Janze <cjanze@christian-janze.com>
2012-03-20 13:35:53 +0100

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -49,5 +49,17 @@ module SessionsHelper
     def clear_return_to
       session.delete(:return_to)
     end
+
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
+    end
+  end
     
 end
